e06a8934803e73138011d12ec1d4eb1c188a47ea
JooHyoung <133327393+jh20000@users.noreply.github.com>
2025-04-09 17:04:50 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.65 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,28 +1,29 @@
 import heapq
 
 def solution(jobs):
-    jobs.sort()  # 요청 시각 기준 정렬
+    # 작업 번호 포함시키기
+    for idx, (req, dur) in enumerate(jobs):
+        jobs[idx] = (req, dur, idx)
+    jobs.sort()
+    
     heap = []
-    
-    time = 0  # 현재 시각
-    idx = 0   # jobs 리스트 인덱스
-    total_time = 0
+    time = 0
+    idx = 0
     count = 0
-    
+    total_time = 0
+
     while count < len(jobs):
-        # 현재 시간까지 요청된 작업들을 모두 heap에 넣음
         while idx < len(jobs) and jobs[idx][0] <= time:
-            req, dur = jobs[idx]
-            heapq.heappush(heap, (dur, req))  # 소요시간 기준 우선순위 큐
+            req, dur, job_idx = jobs[idx]
+            heapq.heappush(heap, (dur, req, job_idx))
             idx += 1
 
         if heap:
-            dur, req = heapq.heappop(heap)
-            time += dur  # 현재 작업 끝나는 시간
-            total_time += time - req  # 반환 시간 누적
+            dur, req, job_idx = heapq.heappop(heap)
+            time += dur
+            total_time += time - req
             count += 1
         else:
-            # 아직 들어온 작업이 없는 경우 -> 다음 작업 시각으로 시간 점프
             time = jobs[idx][0]
 
     return total_time // len(jobs)
